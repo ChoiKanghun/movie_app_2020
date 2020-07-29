d26182bd2c3b342c64ae4d99414fb4c7521edf0e
System Administrator <rkdgns4562@naver.com>
2020-07-29 13:10:41 +0900

    Updates

---
+++ precache-manifest.bb351a7210a5ff06c1e003365ff253b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea8d6f0aa327bb97be88ac586c8b217",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "e02a13edc3fee900d3c9",
+    "url": "/movie_app_2020/static/css/main.47afaf7d.chunk.css"
+  },
+  {
+    "revision": "36cd93b10d7d5fb61658",
+    "url": "/movie_app_2020/static/js/2.9ef3c0e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.9ef3c0e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02a13edc3fee900d3c9",
+    "url": "/movie_app_2020/static/js/main.e613e19d.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
