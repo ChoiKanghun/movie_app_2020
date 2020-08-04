960515a9b740602c60700d8eda8e3e2e4307b07b
최강훈 <rkdgns4562@naver.com>
2020-08-04 17:42:22 +0900

    Add: added test.js

---
+++ src/routes/Test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import axios from "axios";
+
+class Test extends React.Component {
+    state = {
+        isLoading:true,
+        data: []
+    }
+    
+    async componentDidMount() {
+        this.state.data = await axios.get("https://yts.mx/api/v2/list_movies.json");
+        this.setState({isLoading:false});
+    }
+    render() {
+        return <div>hello</div>
+    }
+
+}
+
+export default Test;
